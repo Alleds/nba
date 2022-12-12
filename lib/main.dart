47122465098bfd876ded66243979a59d5116e460
Vitaly Ulyanov <vitalaulyanov@gmail.com>
2022-12-12 16:34:13 +0300

    add device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,16 @@ import 'package:nba/players_list/domain/repository/players_list_repository.dart'
 import 'package:nba/players_list/presentation/screens/start_screen.dart';
 import 'package:provider/provider.dart';
 import './di/injectable.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
   configureDependencies();
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
